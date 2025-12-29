b5b54e4868aece2f2372ead120ba6b9c3834b6e9
Isabella Tressino <isamaria1708@gmail.com>
2025-12-29 01:31:25 -0300

    Add logo to navbar

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -23,7 +23,24 @@ export default function Navbar() {
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 2.2, duration: 0.6, ease: "easeOut" }}
                 className={`fixed top-0 left-0 z-50 w-full transition-colors ease-out duration-500 ${scrolled ? "bg-background/80 backdrop-blur" : "bg-transparent"}`}>
-                <div className="mx-auto max-w-300 flex items-center justify-end px-8 sm:px-10 lg:px-20 py-2 md:py-4">
+                <div className="mx-auto max-w-300 flex items-center justify-between px-8 sm:px-10 lg:px-20 py-2 md:py-4">
+
+                    <motion.a
+                        href="#hero"
+                        className="flex gap-2 items-center"
+                        initial={{ opacity: 0 }}
+                        animate={{
+                            opacity: scrolled ? 1 : 0,
+                            pointerEvents: scrolled ? "auto" : "none",
+                        }}
+                        transition={{ duration: .5, ease: "easeOut" }}
+                    >
+                        <img src="favicon-32x32.png" className="h-6"/>
+                        <span className="uppercase font-display text-text-main">
+                            Isabella Tressino
+                        </span>
+                    </motion.a>
+
 
                     <ul className="hidden sm:flex gap-8 uppercase text-sm font-display">
                         <NavItem href="#about">Sobre mim</NavItem>
