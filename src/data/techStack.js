3af2d8f51834e4f072a2091651d5661ec0ca4787
Isabella Tressino <isamaria1708@gmail.com>
2026-06-17 18:10:16 -0300

    Update TechStack

--- src/data/techStack.js
+++ src/data/techStack.js
@@ -1,6 +1,6 @@
 export const technologies = {
   frontend: ['HTML5', 'CSS3', 'JavaScript', 'React', 'Tailwind CSS', 'Axios', 'Vite'],
   backend: ['Node.js', 'Express', 'MongoDB', 'Prisma', 'Firebase'],
-  ia: ['Python', 'Pandas', 'Scikit-learn', 'Matplotlib'],
+  ia: ['Python', 'Pandas', 'Scikit-learn', 'Matplotlib', "Power BI", "Excel"],
   others: ['Git / Github', 'Figma', 'Kotlin', 'Linux'],
 };
